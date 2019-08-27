41c4f57bbbf2928d9afedb116f47579d4bb685e9
SGK <solene.garda.krebs@gmail.com>
2019-08-27 18:57:27 +0200

    Ajout du formulaire de création de compte et de la base html pour le menu

--- src/Controller/CheckItController.php
+++ src/Controller/CheckItController.php
@@ -14,4 +14,36 @@ class CheckItController extends AbstractController
     {
         return $this->render('logout/login.html.twig');
     }
+
+    /**
+     * @Route("/creer-compte", name="account_creation")
+     */
+    public function printAccountCreationPage() {
+        // Regarder s'il y a des données envoyées par le formulaire
+        $form_submitted = false;
+
+        if ($form_submitted == true) {
+            // Essayer de créer un compte
+            $account_created = false;
+
+            if ($account_created == true) {
+                return $this->render('logout/account_created.html.twig');
+            }
+            else {
+                $error_message = "Erreur lors de la création du compte";
+                $mail_submitted = "bidule@gmail.com";
+
+                return $this->render('logout/account_creation.html.twig', [
+                    'error_message' => $error_message,
+                    'mail_submitted' => $mail_submitted
+                ]);
+            }
+        }
+        else {
+            return $this->render('logout/account_creation.html.twig', [
+                'error_message' => "",
+                'mail_submitted' => ""
+            ]);
+        }
+    }
 }
