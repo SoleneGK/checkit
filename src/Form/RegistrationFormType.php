1558606dca4d32064cbb0831d8e3b34df1565f60
Solène <solene.garda.krebs@gmail.com>
2019-09-05 19:08:06 +0200

    Personnalisation des pages de création de compte et de création de compte Suppression de templates inutiles

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,41 +15,49 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('username')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-        ;
-    }
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('username')
+			->add('email')
+			->add('plainPassword', RepeatedType::class, [
+				// instead of being set onto the object directly,
+				// this is read and encoded in the controller
+				'type' => PasswordType::class,
+				'invalid_message' => 'Les mots de passe doivent être identiques.',
+				'mapped' => false,
+				'options' => [
+					'constraints' => [
+						new NotBlank([
+							'message' => 'Vous devez entrer un mot de passe.',
+						]),
+						new Length([
+							'min' => 8,
+							'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} charactères',
+							// max length allowed by Symfony for security reasons
+							'max' => 4096,
+						]),
+					],
+				],
+				'first_options' => ['label' => 'Mot de passe'],
+				'second_options' => ['label' => 'Confirmer le mot de passe'],
+				'required' => true,
+			])
+			->add('agreeTerms', CheckboxType::class, [
+				'mapped' => false,
+				'constraints' => [
+					new IsTrue([
+						'message' => 'Vous devez accepter les conditions d\'utilisation.',
+					]),
+				],
+			])
+		;
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => User::class,
+		]);
+	}
 }
