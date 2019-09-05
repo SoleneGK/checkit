7d5c758dc83560441d1efd272829cc88f34567c2
Solène <solene.garda.krebs@gmail.com>
2019-09-06 00:10:41 +0200

    Création du formulaire de création de tâche

--- src/Controller/TaskCreationController.php
+++ src/Controller/TaskCreationController.php
@@ -4,43 +4,56 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Form\TaskCreationFormType;
 
 class TaskCreationController extends AbstractController
 {
-    /**
-     * @Route("/taches/ajouter", name="task_creation")
-     */
-    public function printTaskListByPriority() {
-        // Y a-t-il une demande de création de tâche ?
-        $data_send = true;
+	/**
+	 * @Route("/taches/ajouter", name="task_creation")
+	 */
+	public function addTask()
+	{
+		$task = new Task();
+		$form = $this->createForm(TaskCreationFormType::class, $task);
 
-        $data = [
-            'title' => "",
-            'description' => "",
-            'importance' => "",
-            'periodicity' => ""
-        ];
 
-        if ($data_send == true) {
-            $data = [
-                'title' => "Prendre rdv chez le médecin",
-                'description' => "Apporter le carnet de santé",
-                'importance' => "critical",
-                'periodicity' => "none"
-            ]; 
+		return $this->render('app/task_creation.html.twig', [
+			'taskCreationForm' =>$form->createView(),
+		]);
+	}
 
-            $creation_succeeded = true;
+	// public function printTaskListByPriority() {
+	// 	// Y a-t-il une demande de création de tâche ?
+	// 	$data_send = true;
 
-            if ($creation_succeeded == true) {
-                return $this->render('logged/task_creation_succeeded.html.twig');
-            }
-            else {
-                return $this->render('logged/task_creation.html.twig', $data);
-            }
-        }
-        else {
-            return $this->render('logged/task_creation.html.twig', $data);
-        }
-        
-    }
+	// 	$data = [
+	// 		'title' => "",
+	// 		'description' => "",
+	// 		'importance' => "",
+	// 		'periodicity' => ""
+	// 	];
+
+	// 	if ($data_send == true) {
+	// 		$data = [
+	// 			'title' => "Prendre rdv chez le médecin",
+	// 			'description' => "Apporter le carnet de santé",
+	// 			'importance' => "critical",
+	// 			'periodicity' => "none"
+	// 		]; 
+
+	// 		$creation_succeeded = true;
+
+	// 		if ($creation_succeeded == true) {
+	// 			return $this->render('logged/task_creation_succeeded.html.twig');
+	// 		}
+	// 		else {
+	// 			return $this->render('logged/task_creation.html.twig', $data);
+	// 		}
+	// 	}
+	// 	else {
+	// 		return $this->render('logged/task_creation.html.twig', $data);
+	// 	}
+		
+	// }
 }
