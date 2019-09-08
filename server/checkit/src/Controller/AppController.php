2d7f09a21517eaa1847c24d0f3dcc07e9629ff63
Solène Garda-Krebs <solene.garda.krebs@gmail.com>
2019-09-09 00:50:33 +0200

    Création des fichiers de configuration du serveur, réorganisation de l'arborescence

---
+++ server/checkit/src/Controller/AppController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @IsGranted ("IS_AUTHENTICATED_REMEMBERED") 
+ */
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->redirectToRoute('display_task_by_priority');
+    }
+}
---
+++ server/checkit/src/Controller/DisplayTaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @IsGranted ("IS_AUTHENTICATED_REMEMBERED")
+ */
+class DisplayTaskController extends AbstractController
+{
+    /**
+     * @Route("/taches/priorite", name="display_task_by_priority")
+     * @Route("/taches)
+     */
+    public function index()
+    {
+        // Faire 4 requêtes pour récupérer les tâches
+
+        return $this->render('display_task/index.html.twig', [
+            'controller_name' => 'DisplayTaskController',
+        ]);
+    }
+}
