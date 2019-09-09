695721e8f319b8a02bb6a5317d6a9ae3927bbbc7
John Doe <anonimous71@gmail.com>
2019-09-09 13:14:18 +0200

    adding use for annottions in DisplayTaskController

--- sources/app/src/Controller/DisplayTaskController.php
+++ sources/app/src/Controller/DisplayTaskController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 /**
  * @IsGranted ("IS_AUTHENTICATED_REMEMBERED")
