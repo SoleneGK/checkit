7149de469888d579545ac3eb82584ff30fd741dd
Solène <solene.garda.krebs@gmail.com>
2019-09-11 01:15:21 +0200

    Création du contrôleur pour afficher les informations du compte et changer le mot de passe

--- sources/app/src/Form/KnownPasswordModificationType.php
+++ sources/app/src/Form/KnownPasswordModificationType.php
@@ -4,25 +4,31 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class RegistrationFormType extends AbstractType
+class KnownPasswordModificationType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('username')
-			->add('email')
-			->add('plainPassword', RepeatedType::class, [
-				// instead of being set onto the object directly,
-				// this is read and encoded in the controller
+			->add('oldPassword', PasswordType::class, [
+				'mapped' => false,
+				'required' => true,
+				'label' => 'Saisir votre ancien mot de passe',
+				'constraints' => [
+					new UserPassword([
+						'message' => 'Votre mot de passe est incorrect.',
+					])
+				],
+			])
+			->add('password', RepeatedType::class, [
 				'type' => PasswordType::class,
 				'invalid_message' => 'Les mots de passe doivent être identiques.',
 				'options' => [
@@ -38,17 +44,12 @@ class RegistrationFormType extends AbstractType
 						]),
 					],
 				],
-				'first_options' => ['label' => 'Mot de passe'],
-				'second_options' => ['label' => 'Confirmer le mot de passe'],
+				'first_options' => ['label' => 'Entrer le nouveau mot de passe'],
+				'second_options' => ['label' => 'Confirmer le nouveau mot de passe'],
 				'required' => true,
 			])
-			->add('agreeTerms', CheckboxType::class, [
-				'mapped' => false,
-				'constraints' => [
-					new IsTrue([
-						'message' => 'Vous devez accepter les conditions d\'utilisation.',
-					]),
-				],
+			->add('submit', SubmitType::class, [
+				'label' => 'Modifier',
 			])
 		;
 	}
